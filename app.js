8ba043cda85425e6d9bff2fa6cbb91ad86c1e947
Thalukajyoshna <23eg106b57@anurag.edu.in>
2025-10-31 09:35:41 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname,'app_server','views'));
 app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -35,7 +34,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', { title: 'Error' });
 });
 
 module.exports = app;
